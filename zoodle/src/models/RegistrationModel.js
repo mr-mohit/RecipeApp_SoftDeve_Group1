7f3ee8b1d477737b129d371898b18c0e9e60da93
mr-mohit <44066175+mr-mohit@users.noreply.github.com>
2022-07-24 03:59:45 -0400

    Created Registration Model

---
+++ zoodle/src/models/RegistrationModel.js
@@ -0,0 +1,12 @@
+export class RegistrationModel{
+    constructor(account_type,email,password,first_name,last_name,adddress,phone_number) 
+    {
+      this.account_type = account_type;
+      this.email = email;
+      this.password = password;
+      this.first_name = first_name;
+      this.last_name = last_name;
+      this.adddress = adddress;
+      this.phone_number = phone_number;
+    }
+  }
