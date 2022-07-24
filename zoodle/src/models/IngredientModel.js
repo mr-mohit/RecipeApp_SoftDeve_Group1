389dd2820b41d662c70e65ebc058a449d86122e6
mr-mohit <44066175+mr-mohit@users.noreply.github.com>
2022-07-24 03:09:59 -0400

    Created Ingredient Model

---
+++ zoodle/src/models/IngredientModel.js
@@ -0,0 +1,10 @@
+export class IngredientModel{
+    constructor(ingredient_name,ingredient_price,ingredient_amount,ingredient_retailer,retailer_profits) 
+    {
+      this.ingredient_name = ingredient_name;
+      this.ingredient_price = ingredient_price;
+      this.ingredient_amount = ingredient_amount;
+      this.ingredient_retailer = ingredient_retailer;
+      this.retailer_profits = retailer_profits;
+    }
+  }
